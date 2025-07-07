ecc04396b349d6e7641ab4f23eb95f6a0e17b71b
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-07-07 09:43:58 +0100

    frontend updates

--- backend/server.py
+++ backend/server.py
@@ -308,4 +308,4 @@ def experience_upload():
     return jsonify({"status": "success", "message": "Experience saved."})
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, port=5001)
