c2f649a46b549f2e02c4af7efc0f21d4f4d59601
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-07-07 09:41:04 +0100

    frontend updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,12 +14,12 @@ export default defineConfig({
     port: 5173, // or whatever you're using
     allowedHosts: ['lahn-server.eastus.cloudapp.azure.com', "lahn-avatar.uni-giessen.de"], // ✅ add this line
 
-    proxy: {
-      // forward any request that starts with /api
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-      }
-    }
+    // proxy: {
+    //   // forward any request that starts with /api
+    //   '/api': {
+    //     target: 'http://localhost:5001',
+    //     changeOrigin: true,
+    //   }
+    // }
   },
 })
