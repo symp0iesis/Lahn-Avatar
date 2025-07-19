180c10faf5b064da8fc11a730766a6739fac2a1c
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-07-19 23:49:57 +0100

    Updates

---
+++ startup_lahn_avatar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Backend
+screen -L -Logfile ~/backend-logs.0 -dmS backend bash -c 'cd ~/Lahn-Avatar && source lahn_env/bin/activate && 
+python server.py'
+
+# Frontend
+screen -L -Logfile ~/frontend-logs.0 -dmS frontend bash -c 'cd ~/Lahn-Avatar/frontend && npm run dev'
+
+# Caddy
+screen -dmS caddy bash -c 'cd ~/caddy-related && sudo caddy run'
