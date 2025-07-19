5a615e630df5d4deacc5b2ece1cdf6b81f879623
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-07-20 00:13:10 +0100

    Updates

--- startup_lahn_avatar.sh
+++ startup_lahn_avatar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Backend
-screen -L -Logfile ~/backend-logs.0 -dmS backend bash -c 'cd ~/Lahn-Avatar && source lahn_env/bin/activate && python server.py'
+screen -L -Logfile ~/backend-logs.0 -dmS backend bash -c 'cd ~/Lahn-Avatar && source lahn_env/bin/activate && python3 server.py'
 
 # Frontend
 screen -L -Logfile ~/frontend-logs.0 -dmS frontend bash -c 'cd ~/Lahn-Avatar/frontend && npm run dev'
 
 # Caddy
-screen -dmS caddy bash -c 'cd ~/caddy-related && sudo caddy run'
+sudo setcap 'cap_net_bind_service=+ep' /usr/bin/caddy
+screen -dmS caddy bash -c 'cd ~/caddy-related && caddy run'
